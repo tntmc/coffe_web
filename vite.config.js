6acf78774662f3d2821b603a8f5f942b7ea12ed6
TNTMC <tntmc11@gmail.com>
2025-06-06 19:56:14 +0700

    pembaruan-vite

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  vite: {
-    server: {
-      allowedHosts: [' https://70b1-2404-8000-1029-1531-856a-1174-dafe-4b65.ngrok-free.app']
-    }
-  }
+  base: process.env.VITE_BASE_PATH || "/coffe_web",
+  // vite: {
+  //   server: {
+  //     allowedHosts: [' https://70b1-2404-8000-1029-1531-856a-1174-dafe-4b65.ngrok-free.app']
+  //   }
+  // }
 })
